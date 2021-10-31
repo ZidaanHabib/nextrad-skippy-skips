884d71e7fc5d0beb08e7babfc3a059b8e62696cc
Zidaan <zidaanhabib06@gmail.com>
2021-10-31 19:15:27 +0200

    Added separate file to store options menu

---
+++ misc/options.py
@@ -0,0 +1,7 @@
+OPTIONS_MENU = """ 
+1) Calibrate                        6) Stop Slew
+2) Slew to azimuth, elevation       7) Adjust slew speed   
+3) Slew to GPS location             8) Set Azimuth limits  
+4) Slew <l,r,u,d>                   9) Set Elevation limits
+5) Sweep Mode On                    q) Quit.       
+"""          
