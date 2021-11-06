c6e86ec807ee8525327e76c60407165251a68bd8
Zidaan <zidaanhabib06@gmail.com>
2021-11-06 23:42:18 +0200

    Added get az and el command

--- misc/options.py
+++ misc/options.py
@@ -1,7 +1,8 @@
 OPTIONS_MENU = """ 
-1) Calibrate                        6) Stop Slew
-2) Slew to azimuth, elevation       7) Adjust slew speed   
-3) Slew to GPS location             8) Set Azimuth limits  
-4) Slew <l,r,u,d>                   9) Set Elevation limits
-5) Sweep Mode On                    q) Quit.       
+1) Calibrate                        6) Get azimuth and elevation
+2) Slew to azimuth, elevation       7) Stop Slew           
+3) Slew to GPS location             8) Adjust slew speed   
+4) Slew <l,r,u,d>                   9) Set Azimuth limits  
+5) Sweep Mode On                    10) Set Elevation limits
+q) Quit.   
 """          
